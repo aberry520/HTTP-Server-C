e70ff194431b409e5549e6b188530c6d4b509a3c
aberry520 <aberry52098@gmail.com>
2026-04-09 21:14:30 -0400

    Initial commit

---
+++ index.c
@@ -0,0 +1,14 @@
+
+// 1. Create a socket (get a "communication handle")
+// 2. Define the address (where you want to listen)
+// 3. Bind the socket to that address
+// 4. Start listening for connections
+// 5. Wait for a client to connect (BLOCKS here)
+// ---- CONNECTION ESTABLISHED ----
+// 6. Create a buffer to hold incoming data
+// 7. Read data from the client
+// 8. Print what you received
+// 9. (Optional for now) Send a response
+// 10. Close the client connection
+// 11. Close the server (for now, later you'll loop)
+
